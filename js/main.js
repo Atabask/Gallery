7078115956158e5ffd0c36d102a11deba6f04ede
Atabask <87390618+Atabask@users.noreply.github.com>
2022-01-17 12:38:31 +0300

    add-js

---
+++ js/main.js
@@ -0,0 +1,2 @@
+import "./modules/renderCart.js";
+// import "style-loader!css-loader!../css/main.css";
---
+++ js/modules/array.js
@@ -0,0 +1,58 @@
+const carts = [
+    {
+        id: 1,
+        src: './img/1.jpg',
+        title: 'You',
+        type: 'image'
+    },
+    {
+        id: 2,
+        src: '../img/2.jpg',
+        title: 'Buy',
+        type: 'image'
+    },
+    {
+        id: 3,
+        src: '../img/3.png',
+        title: 'Oh, f**k',
+        type: 'image'
+    },
+    {
+        id: 4,
+        src: '../audio/honor.mp3',
+        title: 'Honor',
+        type: 'audio'
+    },
+    {
+        id: 5,
+        src: '../audio/jazzy.mp3',
+        title: 'Jazzy',
+        type: 'audio'
+    },
+    {
+        id: 6,
+        src: '../audio/bandit.mp3',
+        title: 'Bandit',
+        type: 'audio'
+    },
+    {
+        id: 7,
+        src: '../video/cat.mp4',
+        title: 'Cat',
+        type: 'video'
+    },
+    {
+        id: 8,
+        src: '../video/trees.mp4',
+        title: 'Trees',
+        type: 'video'
+    },
+    {
+        id: 9,
+        src: '../video/metallica.mp4',
+        title: 'Metallica',
+        type: 'video'
+    },
+];
+
+export {carts};
