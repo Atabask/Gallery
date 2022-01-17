8409ae11fbf5a776ea8fd57b30442b5f94f8b2aa
Atabask <87390618+Atabask@users.noreply.github.com>
2022-01-17 12:50:45 +0300

    add-js

--- js/modules/array.js
+++ js/modules/array.js
@@ -7,49 +7,49 @@ const carts = [
     },
     {
         id: 2,
-        src: '../img/2.jpg',
+        src: './img/2.jpg',
         title: 'Buy',
         type: 'image'
     },
     {
         id: 3,
-        src: '../img/3.png',
+        src: './img/3.png',
         title: 'Oh, f**k',
         type: 'image'
     },
     {
         id: 4,
-        src: '../audio/honor.mp3',
+        src: './audio/honor.mp3',
         title: 'Honor',
         type: 'audio'
     },
     {
         id: 5,
-        src: '../audio/jazzy.mp3',
+        src: './audio/jazzy.mp3',
         title: 'Jazzy',
         type: 'audio'
     },
     {
         id: 6,
-        src: '../audio/bandit.mp3',
+        src: './audio/bandit.mp3',
         title: 'Bandit',
         type: 'audio'
     },
     {
         id: 7,
-        src: '../video/cat.mp4',
+        src: './video/cat.mp4',
         title: 'Cat',
         type: 'video'
     },
     {
         id: 8,
-        src: '../video/trees.mp4',
+        src: './video/trees.mp4',
         title: 'Trees',
         type: 'video'
     },
     {
         id: 9,
-        src: '../video/metallica.mp4',
+        src: './video/metallica.mp4',
         title: 'Metallica',
         type: 'video'
     },
